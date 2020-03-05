5ae119db26ebdf62dc61c3289bd4a57f78dfa9e8
goharmihranian <mihraniangohar@gmail.com>
2020-03-05 00:12:59 -0800

    week 9 hw complete

---
+++ lab-exercises/week-9/iris-analysis.r/Iris-analysis.r
@@ -0,0 +1,28 @@
+library(datasets)
+data(iris)
+summary(iris)
+names(iris) <- tolower(names(iris))
+library(dplyr)
+virginica <- filter(iris, species == "virginica")
+head(virginica)
+sepalLength6 <- filter(iris, species == "virginica", sepal.length > 6)
+tail(sepalLength6)
+selected <- select(iris, sepal.length, sepal.width, petal.length)
+selected2 <- select(iris, sepal.length:petal.length)
+head(selected, 3)
+identical(selected, selected2)
+newCol <- mutate(iris, greater.half = sepal.width > 0.5 * sepal.length)
+tail(newCol)
+newCol <- arrange(newCol, petal.width)
+head(newCol)
+arr.virg <- newCol %>% filter(species == "virginica") %>%
+  arrange(sepal.width)
+arr.virg[30:35,]
+summarise(arr.virg, mean.length = mean(sepal.length, na.rm = TRUE))
+plot(iris)
+plot(iris$sepal.width, iris$sepal.length)
+hist(iris$sepal.width)
+
+library(ggplot2)
+plot(x=iris$Sepal.Length, y=iris$Sepal.Width, 
+     xlab="Sepal Length", ylab="Sepal Width",  main="Sepal Length-Width") 
